ab2d10abd691ac91e458f918349092d04fb261f6
bernborgess <bernborgess@outlook.com>
2023-02-25 01:17:31 -0300

    clean artists grid

--- src/pages/Artist/AllArtists.tsx
+++ src/pages/Artist/AllArtists.tsx
@@ -1,26 +1,11 @@
 import { Button, Stack } from "@mui/material";
-import { GridCellParams, GridColDef } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
-import { ActionButtons } from "../../components/ActionButtons";
 import { ArtistCard } from "../../components/Cards/ArtistCard";
 import { WaitingFetchCircle } from "../../components/WaitingFetchCircle";
 import { Artist } from "../../constants/artist";
 import { useRouting } from "../../routes";
 import { useApi } from "../../services";
 import { Container } from "./styles";
-
-const artistColumns: Array<GridColDef & { field: keyof Artist }> = [
-  { field: "name", headerName: "Nome", flex: 1 },
-  { field: "phone", headerName: "Telefone", flex: 1 },
-  { field: "website", headerName: "Site", flex: 1 },
-  {
-    field: "id", headerName: "Ações", flex: 1,
-    renderCell: (params: GridCellParams) =>
-      <ActionButtons id={params.value} />
-  }
-];
-
-
 
 
 export function AllArtists() {
@@ -74,17 +59,6 @@ export function AllArtists() {
         </Stack>
       }
 
-      {/* <TableContent>
-        {
-          !artists
-            ? <WaitingFetchCircle />
-            :
-            <DataGridVenus
-              rows={artists}
-              columns={artistColumns}
-            />
-        }
-      </TableContent> */}
 
     </Container>
   );
